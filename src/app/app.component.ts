fd208de27cea9e2e00bf4f114d53b873dfd83ed3
puneeth037h <punithshanakanahalli@gmail.com>
2025-04-13 19:30:03 +0530

    hotelbookingAngular start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './componenets/home/home.component';
+import { HeaderComponent } from './componenets/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HomeComponent,HeaderComponent],
   template: `
     <h1>Welcome to {{title}}!</h1>
-
+    <p>hello</p>
+    
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'first-ng-app';
+  title = 'Booking';
 }
